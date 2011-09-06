fb557ea64f117378de606f99c72da6e1d905b3e0
Arvid E. Picciani <aep@libqxt.org>
2011-09-06 15:21:07 +0200

    change license to BSD 3 clause

--- tools/tr-tool/main.cpp
+++ tools/tr-tool/main.cpp
@@ -1,28 +1,34 @@
-#include <QCoreApplication>
-#include <QLocale>
-#include <QTextStream>
-#include <QDateTime>
-#include <QMetaEnum>
-#include <QDebug>
-#include <QFile>
-#include <QString>
-#include <QStringList>
-
-QString capitalize(const QString& str);
-
-static const char* qxt_header =
-"/****************************************************************************\n"
-" **\n"
-" ** Copyright (C) Qxt Foundation. Some rights reserved.\n"
-" **\n"
-" ** This file is part of the QxtCore module of the Qxt library.\n"
-" **\n"
-" ** This library is free software; you can redistribute it and/or modify it\n"
-" ** under the terms of the Common Public License, version 1.0, as published\n"
-" ** by IBM, and/or under the terms of the GNU Lesser General Public License,\n"
-" ** version 2.1, as published by the Free Software Foundation.\n"
-" **\n"
 " ** This file is provided \"AS IS\", without WARRANTIES OR CONDITIONS OF ANY\n"
+/****************************************************************************
+** Copyright (c) 2006 - 2011, the LibQxt project.
+** See the Qxt AUTHORS file for a list of authors and copyright holders.
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are met:
+**     * Redistributions of source code must retain the above copyright
+**       notice, this list of conditions and the following disclaimer.
+**     * Redistributions in binary form must reproduce the above copyright
+**       notice, this list of conditions and the following disclaimer in the
+**       documentation and/or other materials provided with the distribution.
+**     * Neither the name of the LibQxt project nor the
+**       names of its contributors may be used to endorse or promote products
+**       derived from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+** DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+** DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+** ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+** <http://libqxt.org>  <foundation@libqxt.org>
+*****************************************************************************/
+
 " ** KIND, EITHER EXPRESS OR IMPLIED INCLUDING, WITHOUT LIMITATION, ANY\n"
 " ** WARRANTIES OR CONDITIONS OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR\n"
 " ** FITNESS FOR A PARTICULAR PURPOSE.\n"
