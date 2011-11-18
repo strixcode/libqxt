0a9335875deebabb8bcd0ae6b4b2bda8df3dd8ec
J-P Nurmi <jpnurmi@libqxt.org>
2011-11-18 07:30:53 +0100

    Fixed #100 bug with the QxtCsvModel

--- src/core/qxtcsvmodel.cpp
+++ src/core/qxtcsvmodel.cpp
@@ -321,7 +321,7 @@ bool QxtCsvModel::insertColumn(int col, const QModelIndex& parent)
 bool QxtCsvModel::insertColumns(int col, int count, const QModelIndex& parent)
 {
     if (parent != QModelIndex() || col < 0) return false;
-    emit beginInsertColumns(parent, col, col + count);
+    beginInsertColumns(parent, col, col + count - 1);
     QxtCsvModelPrivate& d_ptr = qxt_d();
     if (col < columnCount())
     {
@@ -339,7 +339,7 @@ bool QxtCsvModel::insertColumns(int col, int count, const QModelIndex& parent)
     for (int i = 0;i < count;i++)
         d_ptr.header.insert(col, "");
     d_ptr.maxColumn += count;
-    emit endInsertColumns();
+    endInsertColumns();
     return true;
 }
 
